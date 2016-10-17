adc048acf32f6a2035811f7200c84280505719d4
wanglaofu <wanglaofu@gmail.com>
2016-10-17 10:56:55 +0800

    Initial Commit

---
+++ MutipleCells/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MutipleCells
+//
+//  Created by la0fu on 16/10/17.
+//  Copyright © 2016年 la0fu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
